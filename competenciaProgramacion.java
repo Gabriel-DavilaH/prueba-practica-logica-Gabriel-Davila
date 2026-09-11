f66f16c6b4015bd49c37d3626442a6940c2ecee7
Jeremy Toalombo <jeremytoalombo3@gmail.com>
2026-09-11 10:24:58 -0500

    Update competenciaProgramacion.java

--- competenciaProgramacion.java
+++ competenciaProgramacion.java
@@ -10,27 +10,6 @@ public class competenciaProgramacion {
         double puntajeBase, penalizacion, bonificacion, puntajeFinal;
         String nivel, observacion;
 
-        System.out.print("Puntaje reto 1: ");
-        reto1 = sc.nextDouble();
-
-        System.out.print("Puntaje reto 2: ");
-        reto2 = sc.nextDouble();
-
-        System.out.print("Puntaje reto 3: ");
-        reto3 = sc.nextDouble();
-
-        System.out.print("Numero de errores: ");
-        errores = sc.nextInt();
-
-        System.out.print("Tiempo total en minutos: ");
-        tiempo = sc.nextInt();
-
-        System.out.print("Resolvio el desafio extra (Si/No): ");
-        desafioExtra = sc.next();
-
-        System.out.print("Descalificado por copia (Si/No): ");
-        descalificado = sc.next();ç
-        
          puntajeBase = reto1 + reto2 + reto3;
 
         penalizacion = errores * 4;
